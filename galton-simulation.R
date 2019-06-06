6be5ac4e53c3875f0be20f810aae07fe4671a463
Jane Pascar <japascar@syr.edu>
2019-06-06 15:24:28 -0400

    Update galton-simulation.R

--- galton-simulation.R
+++ galton-simulation.R
@@ -7,16 +7,16 @@ i = 0
 samp = data.frame(V1 = as.numeric())
 
 while(i == 0) {
-	samp <- rbind(samp, data.frame(V1 = rbinom(1,size=ntrial,prob)))
-	if(nrow(samp)==1) next
+	samp <- rbind(samp, data.frame(V1 = rbinom(1, size = ntrial, prob)))
+	if(nrow(samp) == 1) next
 	fig <- ggplot() +
 		geom_histogram(data = samp, aes(x = V1, y = ..density..), color = "black", fill = "gray80", bins = 30) +
 		stat_function(data = samp, aes(V1),
 			fun = dnorm, n = 100,
-			args = list(mean = prob*ntrial, sd = sqrt(ntrial*prob*(1-prob))),
+			args = list(mean = prob * ntrial, sd = sqrt(ntrial * prob * (1 - prob))),
 			color = "red") +
-		xlim(c(20,80)) +
-		ylim(c(0,.1)) +
+		xlim(c(20, 80)) +
+		ylim(c(0, .1)) +
 		labs(x = "", 
 			y = "Density", 
 			title = paste(nrow(samp), "trials", sep = " "), 
